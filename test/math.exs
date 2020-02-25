f818be1a998efb192e2f8ce6aecdf29c13857fd9
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2020-02-25 11:44:14 -0300

    Function zero with multiple clauses

--- test/math.exs
+++ test/math.exs
@@ -6,7 +6,20 @@ defmodule Math do
   defp do_sum(a, b) do
     a + b
   end
+
+  def zero?(0) do
+    true
+  end
+
+  def zero?(x) when is_integer(x) do
+    false
+  end
 end
 
 IO.puts(Math.sum(2, 40))
-IO.puts(Math.do_sum(2, 40))
+# IO.puts(Math.do_sum(2, 40))
+
+IO.puts(Math.zero?(0))
+IO.puts(Math.zero?(1))
+# IO.puts(Math.zero?([1, 2, 3]))
+IO.puts(Math.zero?(0.0))
