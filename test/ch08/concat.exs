2b7309d10dc71fd38660fe02f105176e53a701d1
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2020-02-25 12:18:21 -0300

    Function join with default arguments and multiple clauses #3

--- test/ch08/concat.exs
+++ test/ch08/concat.exs
@@ -1,5 +1,11 @@
 defmodule Concat do
-  def join(a, b, sep \\ " ") do
+  def join(a, b \\ nil, sep \\ " ")
+
+  def join(a, b, _sep) when is_nil(b) do
+    a
+  end
+
+  def join(a, b, sep) do
     a <> sep <> b
   end
 end
@@ -12,3 +18,4 @@ end
 
 IO.puts(Concat.join("Hello", "world"))
 IO.puts(Concat.join("Hello", "world", "-"))
+IO.puts(Concat.join("Hello"))
